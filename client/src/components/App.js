92c952be20d1542905aadcd69dcc24e36bc37c92
sharunspi <sharunkk1997@gmail.com>
2021-08-31 21:42:47 +0530

    added router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { BrowserRouter,Route} from 'react-router-dom'
-
-const hai = () =>{
-    return <div>
-        hai
-    </div>
-}
+import StreamList from './streams/StreamList'
+import StreamDelete from './streams/StreamDelete'
+import StreamEdit from './streams/StreamEdit'
+import StreamShow from './streams/StreamShow'
+import StreamCreate from './streams/StreamCreate'
 
 export default function App() {
     
     return (
         <div>
             <BrowserRouter>
-            <Route path='/' exact component={hai}/>
+            <Route path='/' exact component={StreamList}/>
+            <Route path='/streams/show' exact component={StreamShow}/>
+            <Route path='/streams/edit' exact component={StreamEdit}/>
+            <Route path='/streams/new' exact component={StreamCreate}/>
+            <Route path='/streams/delete' exact component={StreamDelete}/>
             </BrowserRouter>
         </div>
     )
