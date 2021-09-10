64c445a027e52f8a06a76ce8de8389dbf7d26b57
sharunspi <sharunkk1997@gmail.com>
2021-09-10 09:19:35 +0530

    added form submit

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,16 +1,50 @@
-import React, { Component } from 'react'
-import { Field, reduxForm } from 'redux-form'
+import React, {Component} from 'react'
+import {Field, reduxForm} from 'redux-form'
+import {Form, Col, Row} from 'react-bootstrap'
 
 class StreamCreate extends Component {
+
+    renderInputs({input, label, mutedText}) {
+        return <div>
+            <Form.Label> {label} </Form.Label>
+            <Form.Control {...input}/>
+            <Form.Text className="text-muted">
+                { mutedText }
+            </Form.Text>
+        </div>
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+    }
+
+
     render() {
         return (
             <div>
-                
+                <Form onSubmit={this.props.handleSubmit(this.onSubmit)} >
+                    <Row>
+                        <Col>
+                            <Form.Group className="mb-3 m-2" controlId="formBasicEmail">
+                                <Field name='stream_name' mutedText='name should be unique' label='Stream name' placeholder='Type stream name' type='text'
+                                    component={
+                                        this.renderInputs
+                                    }/>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="mb-3 m-2" controlId="formBasicEmail">
+                                <Field name='stream_type' mutedText='name should be unique' label='Stream Description' placeholder='Type stream description' type='text'
+                                    component={
+                                        this.renderInputs
+                                    }/>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                </Form>
             </div>
         )
     }
 }
 
-export default reduxForm({
-    form : 'streamCreate'
-})(StreamCreate) 
+export default reduxForm({form: 'streamCreate'})(StreamCreate)
